898249f0f38067a434418de1e6092970dbf389a6
Tanner123g <tanner123gleason@gmail.com>
2025-09-17 21:34:40 -0400

    feat: account_number and routing_number added

--- Bank/checkingaccount.py
+++ Bank/checkingaccount.py
@@ -2,8 +2,8 @@ from bankaccount import BankAccount
 
 class CheckingAccount(BankAccount):
 
-    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
-        super().__init__(customer_name, current_balance, minimum_balance)
+    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit, account_number, routing_number):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.transfer_limit = transfer_limit
 
     def withdraw(self, amount):
