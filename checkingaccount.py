f3741fcfa33b19ecde775517061faaffeeee8e6b
jbotticc <jbotticc@charlotte.edu>
2025-09-17 20:40:30 -0400

    feat: added checking account class

---
+++ checkingaccount.py
@@ -0,0 +1,20 @@
+from bankaccount import BankAccount
+
+class CheckingAccount(BankAccount):
+
+    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.transfer_limit = transfer_limit
+
+    def withdraw(self, amount):
+        if self.current_balance - amount < self.minimum_balance:
+            print(f"Cannot withdraw ${amount}: Not enough money in balance\n")
+        elif amount > self.transfer_limit:
+            print(f"Cannot withdraw ${amount}: Amount specified '${amount}' is above transfer limit\n")
+        else:
+            super().withdraw(amount)
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print("Account Type: Checking Account")
+        print(f"Transfer Limit: {self.transfer_limit:.2f}\n")
